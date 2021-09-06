895ec2cf39a5759376dca1882cf202ba9554ac59
Matheus Augusto <matheuspinho2000@hotmail.com>
2021-09-05 21:42:38 -0300

    Fix

--- testa_velha.cpp
+++ testa_velha.cpp
@@ -9,14 +9,16 @@
 #include "catch.hpp"
   
 
-
-TEST_CASE( "Testa velha", "[single-file]" ) {
-	int teste1[3][3]= {   { 2, 0, 1 }, 
-	                      { 2, 0, 1 },
-						  { 0, 2, 1 }
-					  };
-    REQUIRE( VerificaVelha(teste1) == 1 );
-
-
+// Test case 1 
+TEST_CASE( "Jogo tem ganhador" ) {
+	WHEN(" X é o vencedor "){
+		THEN("X ganha na coluna 3"){
+			int teste1[3][3]= { { 2, 0, 1 }, 
+	                      		{ 2, 0, 1 },
+						  		{ 0, 2, 1 }
+					  						};
+    		REQUIRE( VerificaVelha(teste1) == 1 );
+		}
+	}
 }
 
