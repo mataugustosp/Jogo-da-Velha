add75b2f4db139edefa94240a6ca1f225a9067cb
Matheus Augusto <matheuspinho2000@hotmail.com>
2021-09-05 22:43:51 -0300

    Refactoring code for test case 2 "Jogo empatado"

--- testa_velha.cpp
+++ testa_velha.cpp
@@ -62,7 +62,7 @@ TEST_CASE( "Jogo tem ganhador" ) {
 // Test case 2
 TEST_CASE( "Jogo empatado" ){
 	int teste1[3][3]= { { 2, 0, 1 }, 
-	                    { 2, 0, 1 },
+	                    { 0, 2, 0 },
 						{ 0, 2, 1 }};
     REQUIRE( VerificaVelha(teste1) == 0 );
 }
