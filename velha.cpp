26347a255173910f763033e12631fe4ca2c44656
Matheus Augusto <matheuspinho2000@hotmail.com>
2021-09-05 21:41:10 -0300

    Teste Case 1 Code

--- velha.cpp
+++ velha.cpp
@@ -15,7 +15,44 @@
 
 int VerificaVelha( int velha[3][3] )
 {
-	return 0; /*!< retorna zero para teste */
+	// Verifica se o X venceu 
+	if(Vence(velha,1)){
+		return 1;
+	}
+	else{
+		return 0; /*!< retorna zero para teste */
+	}
+}
+
+/*!< Funcao usada para verificar e retornar se X ou O venceu */
+int Vence(int velha[3][3], int numero){
+	if(velha[0][0] == numero && velha[1][1] == numero && velha[2][2] == numero){
+		return 1;
+	}
+	else if(velha[0][2] == numero && velha[1][1] == numero && velha[2][0] == numero){
+		return 1;
+	}
+	else if(velha[0][0] == numero && velha[1][0] == numero && velha[2][0] == numero){
+		return 1;
+	}
+	else if(velha[0][1] == numero && velha[1][1] == numero && velha[2][1] == numero){
+		return 1;
+	}
+	else if(velha[0][2] == numero && velha[1][2] == numero && velha[2][2] == numero){
+		return 1;
+	}
+	else if(velha[0][0] == numero && velha[0][1] == numero && velha[0][2] == numero){
+		return 1;
+	}
+	else if(velha[1][0] == numero && velha[1][1] == numero && velha[1][2] == numero){
+		return 1;
+	}
+	else if(velha[2][0] == numero && velha[2][1] == numero && velha[2][2] == numero){
+		return 1;
+	}
+	else{
+		return 0;
+	}
 }
 
 
--- velha.hpp
+++ velha.hpp
@@ -1,3 +1,5 @@
 int VerificaVelha( int velha[3][3] );
 
+int Vence(int velha[3][3], int numero);
+
 
