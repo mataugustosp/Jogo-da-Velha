97cb4becb8ee33a42ad0b5b6f67311312bc637cf
Matheus Augusto <matheuspinho2000@hotmail.com>
2021-09-05 21:46:43 -0300

    New test Cases "X ganha na diagonal" and "X ganha na diagonal"

--- testa_velha.cpp
+++ testa_velha.cpp
@@ -12,10 +12,24 @@
 // Test case 1 
 TEST_CASE( "Jogo tem ganhador" ) {
 	WHEN(" X é o vencedor "){
-		THEN("X ganha na coluna 3"){
+		THEN("X ganha na vertical"){
 			int teste1[3][3]= { { 2, 0, 1 }, 
 	                      		{ 2, 0, 1 },
 						  		{ 0, 2, 1 }
+					  						};
+    		REQUIRE( VerificaVelha(teste1) == 1 );
+		}
+		THEN("X ganha na diagonal"){
+			int teste1[3][3]= { { 1, 0, 0 }, 
+	                      		{ 2, 1, 2 },
+						  		{ 0, 2, 1 }
+					  						};
+    		REQUIRE( VerificaVelha(teste1) == 1 );
+		}
+		THEN("X ganha na horizontal"){
+			int teste1[3][3]= { { 1, 1, 1 }, 
+	                      		{ 2, 0, 2 },
+						  		{ 0, 2, 0 }
 					  						};
     		REQUIRE( VerificaVelha(teste1) == 1 );
 		}
